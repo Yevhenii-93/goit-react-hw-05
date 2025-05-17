33c3f09281858ed3aa49d98296ddaf3a3a1d06fc
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-05-18 00:54:49 +0300

    add movies page

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,8 +1,6 @@
 import css from "./MoviesList.module.css";
 
 export default function MoviesList({ topik }) {
-  console.log(topik);
-
   return (
     <li className={css.container}>
       <h3>{topik.title}</h3>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import { fetchTopFilmsApi } from "../../services/services.js";
+import css from "./MoviesPage.module.css";
+import MoviesList from "../../components/MoviesList/MoviesList.jsx";
+
 export default function MoviesPage() {
-  return <div>Movies Page</div>;
+  const [topik, setTopik] = useState([]);
+
+  useEffect(() => {
+    async function fetchTopikMovies() {
+      try {
+        const newTopik = await fetchTopFilmsApi();
+        setTopik(newTopik);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchTopikMovies();
+  }, []);
+
+  return (
+    <div>
+      <h2 className={css.title}>TOP 20 today</h2>
+      {topik.length > 0 && (
+        <ul className={css.list}>
+          {topik.map((topik) => (
+            <MoviesList key={topik.id} topik={topik} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
