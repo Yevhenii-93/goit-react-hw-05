83ba7df522ce961136fc977433f75ca59169930a
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-05-21 20:16:14 +0300

    fix

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import css from "./MoviesList.module.css";
+import css from "./MovieList.module.css";
 
 export default function MoviesList({ topik }) {
   const location = useLocation();
